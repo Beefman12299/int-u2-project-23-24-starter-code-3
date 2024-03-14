ff0c93743a896d75426b04fc1f6260abbe587600
Beefman12299 <143620348+Beefman12299@users.noreply.github.com>
2024-03-14 14:40:56 +0000

    3/14/24

---
+++ script.js/script.js
@@ -0,0 +1,9 @@
+let happy= document.querySelector(".Happybutton");
+let sad= document.querySelector(".Sadbutton");
+let hight1= document.querySelector(".hight1")
+let hight2=document.querySelector(".hight2")
+let submit=document.querySelector(".submit")
+
+happy.addEventListener("click",function(){
+    input=happy;
+});
