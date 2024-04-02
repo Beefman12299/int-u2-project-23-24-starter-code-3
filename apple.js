2c9580d5cb81c965252ad56062ac44357fc781ea
Beefman12299 <143620348+Beefman12299@users.noreply.github.com>
2024-04-02 13:45:26 +0000

    4

--- apple.js
+++ apple.js
@@ -17,18 +17,29 @@ Q1="sad";
 });
 
 hight1.addEventListener("click",function(){
-Q2=hight1;
+Q2="hight1";
 });
 
 hight2.addEventListener("click",function(){
-Q2=hight2
+Q2="hight2"
 });
 
 submit.addEventListener("click",function(){
     let message=document.querySelector(".message")
-if(Q1===happy && Q2===hight2) {message.innerHTML="you are the tie shirt!";}
-else if (Q1===happy && Q2===hight1){message.innerHTML="you are the rick and morty shirt!";}
-else if (Q1===sad && Q2===hight1){message.innerHTML="you are the cool anime shirt!";}
-else if (Q1===sad && Q2===hight2){message.innerHTML="you are the spongebob shirt!";}
+
+    if(Q1==="happy" && Q2==="hight2") {message.innerHTML=
+    `<h3>you are the tie shirt!</h3>
+    <img src="tieshirt.png">` ;}
+
+    else if (Q1==="happy" && Q2==="hight1"){message.innerHTML=
+    `<h3>you are the rick and morty shirt!</h3>
+    <img src="ricknmorty.png">`;}
+
+else if (Q1==="sad" && Q2==="hight1"){message.innerHTML=
+    `<h3>you are the cool anime shirt!</h3>
+    <img src="anime.jpg">`;}
+
+    else if (Q1==="sad" && Q2==="hight2"){message.innerHTML=
+  `<h3>you are the spongebob shirt!</h3>
+  <img src="spongbob.png">`  ;}
 });
-console.log (happy)
